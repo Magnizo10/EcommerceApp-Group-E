e32813074e07efdfeddd5539c143c8162602d44c
Mihlali Yono <mihlaliyono@gmail.com>
2025-10-26 17:19:59 +0200

    Added Product Page with navBar

---
+++ Ecommerce-App/src/app/product-page/product-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product-page',
+  templateUrl: './product-page.component.html',
+  styleUrls: ['./product-page.component.css'],
+  standalone: false,
+})
+export class ProductPageComponent {
+
+}
