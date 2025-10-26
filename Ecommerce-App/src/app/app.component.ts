6b635f7c2d972f596cda9576c81f84519b85b5e5
manigzo <thulig.mphungulo@gmail.com>
2025-10-26 21:06:10 +0200

    Homepage

--- Ecommerce-App/src/app/app.component.ts
+++ Ecommerce-App/src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductservService } from './services/productserv.service';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
   title = 'Ecommerce-App';
    // The following code demonstrates how to use the ProductservService to fetch products *testing purpose
   products: any[] = [];
