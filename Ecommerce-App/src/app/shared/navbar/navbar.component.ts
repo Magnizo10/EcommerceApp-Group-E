c872f9e42bf6fcaec9e44649fca6118f529b1d91
Magnizo10 <thulig.mphungulo@gmail.com>
2025-10-28 00:14:00 +0200

    adding search and changing cart icon

--- Ecommerce-App/src/app/shared/navbar/navbar.component.ts
+++ Ecommerce-App/src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductservService } from '../../services/productserv.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  
+  searchTerm = '';
+  searchResults: any[] = [];
+  showResults = false;
+
+  constructor(private productService: ProductservService) {}
+
+  onSearch() {
+    if (this.searchTerm.trim()) {
+      this.productService.getProducts().subscribe(products => {
+        this.searchResults = products.filter(product =>
+          product.title.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+          product.category.toLowerCase().includes(this.searchTerm.toLowerCase())
+        );
+        this.showResults = true;
+      });
+    }
+  }
+
+  hideResults() {
+    setTimeout(() => this.showResults = false, 200);
+  }
 }
